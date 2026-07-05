7da02e04243765484c851caf461fafc7dcb2ebfd
taha <297468732+name-abacus-mud@users.noreply.github.com>
2026-07-05 11:43:14 +0530

    my game is finally working
    really happy that i could make my game work
    without taking help from google
    or ai !!
    
    i am so so happy!!

--- game.js
+++ game.js
@@ -1,7 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-//  this is for keeping the score for two players
-
 function getComputerChoice(no) {
     num = Math.floor(Math.random()*no);
 
@@ -18,40 +14,48 @@ function getComputerChoice(no) {
 
 function getHumanChoice() {
     choice = prompt('please enter your choice!!');
+    lowercaseChoice = choice.toLowerCase();
+    // making the input case sensitive
 
-    return choice
+    return lowercaseChoice
 }
 // this will take input from the human and return it 
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase(); 
-    // making the input case-insensitive
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    //  this is for keeping the score for two players
 
-    if (humanChoice=='rock' && computerChoice=='paper') {
-        console.log('You lose! Paper beats Rock')
-        ++computerScore
-    } else if (humanChoice=='paper' && computerChoice=='rock') {
-        console.log('You win! Paper beats Rock')
-        ++humanScore
-    } else if (humanChoice=='rock' && computerChoice=='scissors') {
-        console.log('You win! Rock beats Scissors')
-        ++humanScore
-    } else if (humanChoice=='scissors' && computerChoice=='rock') {
-        console.log('You lose! Rock beats Scissors')
-        ++computerScore
-    } else if (humanChoice=='paper' && computerChoice=='scissors') {
-        console.log('You lose! Scissors beats Paper')
-        ++computerScore
-    } else if (humanChoice=='scissors' && computerChoice=='paper') {
-        console.log('You win! Scissors beats Paper')
-        ++humanScore
+    function playRound(humanChoice, computerChoice) { 
+
+        if (humanChoice=='rock' && computerChoice=='paper') {
+            console.log('You lose! Paper beats Rock')
+            ++computerScore;
+        } else if (humanChoice=='paper' && computerChoice=='rock') {
+            console.log('You win! Paper beats Rock')
+            ++humanScore;
+        } else if (humanChoice=='rock' && computerChoice=='scissors') {
+            console.log('You win! Rock beats Scissors')
+            ++humanScore;
+        } else if (humanChoice=='scissors' && computerChoice=='rock') {
+            console.log('You lose! Rock beats Scissors')
+            ++computerScore;
+        } else if (humanChoice=='paper' && computerChoice=='scissors') {
+            console.log('You lose! Scissors beats Paper')
+            ++computerScore;
+        } else if (humanChoice=='scissors' && computerChoice=='paper') {
+            console.log('You win! Scissors beats Paper')
+            ++humanScore;
+        } else {
+            console.log('you both chose the same thing! try again!')
+        }
     }
-    // lots of if else lol
+
+    playRound(getHumanChoice(), getComputerChoice(3));
+    playRound(getHumanChoice(), getComputerChoice(3));
+    playRound(getHumanChoice(), getComputerChoice(3));
+    playRound(getHumanChoice(), getComputerChoice(3));
+    playRound(getHumanChoice(), getComputerChoice(3));
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice(3);
-// i don't know why const is used here /sob
-
-playRound(humanSelection, computerSelection);
+playGame();
