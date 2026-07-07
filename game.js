28cfb4d2119cf08d6abc1abdd099fca9925e763d
taha <297468732+name-abacus-mud@users.noreply.github.com>
2026-07-07 13:19:21 +0530

    I finished the project!!
    this one was quick!! I only did the barebones
    and nothing extra.. this will help learn
    faster and not strive for perfection
    never strive for perfection. strive for
    consistency!!

--- game.js
+++ game.js
@@ -1,55 +1,74 @@
 // this will get the choice from the computer
-function getComputerChoice(no) {
-    num = Math.floor(Math.random()*no);
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
 
-    switch(num) {
+    switch (num) {
         case 0:
-            return 'rock'
+            return 'rock';
         case 1:
-            return 'paper'
+            return 'paper';
         case 2:
-            return 'scissors'
+            return 'scissors';
     }
 }
 
-// this will take input from the human and return it 
-function getHumanChoice() {
-    choice = prompt('please enter your choice!!');
-    lowercaseChoice = choice.toLowerCase();
-    // making the input case sensitive
+// connecting score and result divs to js file here
+const displayScore = document.querySelector('#score');
+const displayResult = document.querySelector('#result');
 
-    return lowercaseChoice
-}
+//  this is for keeping the score for two players
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    //  this is for keeping the score for two players
+// this is the function for to play the game round
+function playRound(humanChoice, computerChoice) { 
+    if (humanChoice=='rock' && computerChoice=='paper') {
+        displayScore.textContent = 'You lose! Paper beats Rock'
+        ++computerScore;
+    } else if (humanChoice=='paper' && computerChoice=='rock') {
+        displayScore.textContent = 'You win! Paper beats Rock'
+        ++humanScore;
+    } else if (humanChoice=='rock' && computerChoice=='scissors') {
+        displayScore.textContent = 'You win! Rock beats Scissors'
+        ++humanScore;
+    } else if (humanChoice=='scissors' && computerChoice=='rock') {
+        displayScore.textContent = 'You lose! Rock beats Scissors'
+        ++computerScore;
+    } else if (humanChoice=='paper' && computerChoice=='scissors') {
+        displayScore.textContent = 'You lose! Scissors beats Paper'
+        ++computerScore;
+    } else if (humanChoice=='scissors' && computerChoice=='paper') {
+        displayScore.textContent = 'You win! Scissors beats Paper'
+        ++humanScore;
+    } else {
+        displayScore.textContent = 'you both chose the same thing! try again!';
+    }
 
-    function playRound(humanChoice, computerChoice) { 
+    displayResult.textContent = `computer: ${computerScore}     you: ${humanScore}`;
 
-        if (humanChoice=='rock' && computerChoice=='paper') {
-            console.log('You lose! Paper beats Rock')
-            ++computerScore;
-        } else if (humanChoice=='paper' && computerChoice=='rock') {
-            console.log('You win! Paper beats Rock')
-            ++humanScore;
-        } else if (humanChoice=='rock' && computerChoice=='scissors') {
-            console.log('You win! Rock beats Scissors')
-            ++humanScore;
-        } else if (humanChoice=='scissors' && computerChoice=='rock') {
-            console.log('You lose! Rock beats Scissors')
-            ++computerScore;
-        } else if (humanChoice=='paper' && computerChoice=='scissors') {
-            console.log('You lose! Scissors beats Paper')
-            ++computerScore;
-        } else if (humanChoice=='scissors' && computerChoice=='paper') {
-            console.log('You win! Scissors beats Paper')
-            ++humanScore;
-        } else {
-            console.log('you both chose the same thing! try again!')
-        }
+    if (humanScore==5) {
+        displayResult.textContent = 'you won the game!!';
+    } else if(computerScore==5) {
+        displayResult.textContent = 'you lost and the computer won!!';
     }
 }
 
-playGame();
+
+// adding event listener to the buttons using event delegation
+let menu = document.querySelector('#menu');
+
+menu.addEventListener('click', (event) => {
+    let target = event.target
+
+    switch (target.id) {
+        case 'rock':
+            playRound('rock', getComputerChoice());
+            break;
+        case 'paper':
+            playRound('paper', getComputerChoice());
+            break;
+        case 'scissors':
+            playRound('scissors', getComputerChoice());
+            break;
+    }
+})
